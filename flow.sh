80fe05c3cc92acb47661218d931bb19f6829097a
Srenevasan Nachiyapan <srene.mn@edgenetworks.in>
2018-03-30 11:29:22 +0530

    test

--- flow.sh
+++ flow.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get install python-pip
 sudo pip install pipenv
 sudo pipenv shell
-sudo pipenv install python-nomad
-
+sudo pip install python-nomad
+python python_regsiter_job.py
 
 
